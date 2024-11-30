09329b13f58ad4c758fde695f5ebd7dc93969108
Craig McLellen <craig.mclellen@insight.com>
2024-12-01 06:56:02 +0800

    first commit

--- MyTicketMaster.Api/Endpoints/EventEndpoints.cs
+++ MyTicketMaster.Api/Endpoints/EventEndpoints.cs
@@ -8,7 +8,17 @@ namespace MyTicketMaster.Api.Endpoints
         {
             var eventsMapGroup = app.MapGroup("/events");
             
-            eventsMapGroup.MapPost("/", () => "Hello World!").MapToApiVersion(1);
+            eventsMapGroup.MapGet("/", () => "Hello World!")
+                .WithName("GetEvents")
+                .MapToApiVersion(1)
+                .Produces(StatusCodes.Status500InternalServerError)
+                .WithTags("Get123")
+                .WithOpenApi(op =>
+                {
+                    op.Summary = "Get all events";
+
+                    return op;
+                }); ;
         }
     }
 }
--- MyTicketMaster.Api/Program.cs
+++ MyTicketMaster.Api/Program.cs
@@ -6,7 +6,13 @@ var builder = WebApplication.CreateBuilder(args);
 builder.AddServiceDefaults();
 
 var services = builder.Services;
-services.AddCarter()
+services
+    .AddCarter()
+    .AddEndpointsApiExplorer()
+    .AddSwaggerGen(c =>
+    {
+        c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "My API", Version = "v1" });
+    })
     .AddApiVersioning(options => {
         options.DefaultApiVersion = new ApiVersion(1);
         options.ApiVersionReader = new UrlSegmentApiVersionReader();
@@ -16,6 +22,15 @@ services.AddCarter()
     });
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("v1/swagger.json", "My API V1");
+    });
+}
 
 app.MapDefaultEndpoints();
 
