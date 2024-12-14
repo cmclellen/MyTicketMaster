5f37759a054aa1b4dac27ff1f7962af43f569eb3
Craig McLellen <craig.mclellen@insight.com>
2024-12-14 14:37:32 +0800

    Fix

--- Api/MyTicketMaster.Web/Program.cs
+++ Api/MyTicketMaster.Web/Program.cs
@@ -1,5 +1,8 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using MyTicketMaster.Web.Clients;
 using MyTicketMaster.Web.Components;
+using System.Net.Http.Headers;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,8 +14,39 @@ builder.Services.AddRazorComponents()
 var httpClientBuilder = builder.Services.AddHttpClient<EventsClientFactory>((sp, httpClient) =>
 {
     httpClient.BaseAddress = new Uri("https://localhost:7243");
+    //httpClient.DefaultRequestHeaders.Authorization =
+    //    new AuthenticationHeaderValue("Bearer", "Your Oauth token");
 });
 builder.Services.AddTransient(sp => sp.GetRequiredService<EventsClientFactory>().GetClient());
+
+var authServerUrl = "http://localhost:8080/";
+var realm = "MyTicketMaster";
+var openIdConnectUrl = $"{authServerUrl}realms/{realm}/";
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme =
+        CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme =
+        OpenIdConnectDefaults.AuthenticationScheme;
+})
+.AddCookie(CookieAuthenticationDefaults.AuthenticationScheme)
+.AddOpenIdConnect(OpenIdConnectDefaults.AuthenticationScheme, options =>
+{
+    options.SignInScheme = "Cookies";
+    options.Authority = openIdConnectUrl;
+    options.ClientId = "ticketing-pvt-client";
+    options.ClientSecret = "oH5Xsgyz6RNwjXuUOtTeWCM46mcGNl4f";
+    options.ResponseType = "code";
+    options.UsePkce = true;
+    options.SaveTokens = true;
+    options.CallbackPath = "/signin-oidc";
+    options.SignedOutCallbackPath = "/signout-callback-oidc";
+    options.RequireHttpsMetadata = false;
+    options.GetClaimsFromUserInfoEndpoint = true;
+});
+
+builder.Services.AddAuthorizationCore();
+builder.Services.AddCascadingAuthenticationState();
 
 var app = builder.Build();
 
@@ -28,11 +62,15 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-
 app.UseAntiforgery();
 
 app.MapStaticAssets();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapBlazorHub().RequireAuthorization();
 
 app.Run();
